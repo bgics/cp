3ded903f6b5bdf87e2e762174b6239a168359cea
Bhuvansh Goyal <bhuvansh.iitb@gmail.com>
2026-06-19 12:48:37 +0530

    Added question: money sums using DP (CSES) (review once)

--- src/money_sums.cpp
+++ src/money_sums.cpp
@@ -3,6 +3,35 @@ using namespace std;
 
 #define ll long long
 
+void solve(vector<int>& coins, vector<int>& solution) {
+  int max_sum = 0;
+  for (auto coin : coins) {
+    max_sum += coin;
+  }
+
+  vector<vector<bool>> possible(max_sum + 1,
+                                vector<bool>(coins.size() + 1, false));
+
+  possible[0] = vector<bool>(coins.size() + 1, true);
+
+  for (int sum = 1; sum <= max_sum; sum++) {
+    for (size_t k = 1; k <= coins.size(); k++) {
+      if (sum >= coins[k - 1]) {
+        possible[sum][k] =
+            possible[sum - coins[k - 1]][k - 1] || possible[sum][k - 1];
+      } else {
+        possible[sum][k] = possible[sum][k - 1];
+      }
+    }
+  }
+
+  for (int i = 1; i <= max_sum; i++) {
+    if (possible[i][coins.size()]) {
+      solution.push_back(i);
+    }
+  }
+}
+
 int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
@@ -11,23 +40,16 @@ int main() {
   cin >> n;
 
   vector<int> coins(n);
-  set<int> sums;
-
   for (int i = 0; i < n; i++) {
     cin >> coins[i];
   }
 
-  for (auto coin : coins) {
-    set<int> new_sums;
-    new_sums.insert(coin);
-    for (auto sum : sums) {
-      new_sums.insert(sum + coin);
-    }
-    sums.merge(new_sums);
-  }
+  vector<int> solution;
 
-  cout << sums.size() << "\n";
-  for (auto sum : sums) {
-    cout << sum << " ";
+  solve(coins, solution);
+
+  cout << solution.size() << "\n";
+  for (auto value : solution) {
+    cout << value << " ";
   }
 }
